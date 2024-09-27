99a37d39e593bbb82866fc037ad4088f9fc93351
soyart <artdrawin@gmail.com>
2024-09-27 17:39:34 +0000

    build yn (c)

--- flake.nix
+++ flake.nix
@@ -3,19 +3,47 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs }:
     let
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+      version = builtins.substring 0 8 lastModifiedDate;
+
       pkgs = nixpkgs.legacyPackages.x86-64.linux;
+
+      # The set of systems to provide outputs for
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      # A function that provides a system-specific Nixpkgs for the desired systems
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
     in
     {
-      devShells.default = {
-        buildInputs = with pkgs; [
-          ccls
+      packages = forAllSystems ({ pkgs }: {
+        default = pkgs.stdenv.mkDerivation {
+          inherit version;
 
+          pname = "yn";
+          src = ./.;
+
+          buildPhase = ''
+            cc main.c
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin;
+            cp a.out $out/bin/yn;
+          '';
+        };
+      });
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
           go
           gopls
           gotools
           go-tools
+
           clang
         ];
       };
